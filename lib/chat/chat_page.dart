72390bc437665fe4883b6853ba341c285caa0bb0
Solbjor <s0lbjorandr3w@gmail.com>
2024-05-12 14:52:46 -0700

    attempting to display the chat
    attempting to display chat names

--- lib/chat/chat_page.dart
+++ lib/chat/chat_page.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:medipal/chat/chat_service.dart';
 import 'package:medipal/objects/message.dart';
+import 'package:medipal/objects/practitioner.dart'; // Import Practitioner object
 
 class ChatPage extends StatefulWidget {
   final String receiverUid;
@@ -9,6 +11,7 @@ class ChatPage extends StatefulWidget {
     super.key,
     required this.receiverUid,
   });
+
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
@@ -17,11 +20,48 @@ class _ChatPageState extends State<ChatPage> {
   final TextEditingController _messageController = TextEditingController();
   final ChatService _chatService = ChatService();
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final DatabaseReference _database = FirebaseDatabase.instance.reference();
+  Practitioner? _currentPractitioner;
+  Practitioner? _receiverPractitioner;
+
+  // Get practitioner
+  Future<Practitioner?> getPractitioner(String uid) async {
+    DatabaseReference ref = FirebaseDatabase.instance.ref('users');
+    DataSnapshot snapshot = await ref.child(uid).get();
+    if (!snapshot.exists) return null;
+    Map<String, dynamic>? value = snapshot.value as Map<String, dynamic>;
+
+    if (value != null) {
+      return Practitioner.fromMap(value);
+    } else {
+      return null;
+    }
+  }
+
+  // Get the current user's name
+  Future<String> _getCurrentUserName() async {
+    Practitioner? currentPractitioner = await getPractitioner(_firebaseAuth.currentUser!.uid);
+    return currentPractitioner?.name ?? 'Anonymous';
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    _currentPractitioner = await getPractitioner(_firebaseAuth.currentUser!.uid) as Practitioner?;
+    _receiverPractitioner = await getPractitioner(widget.receiverUid) as Practitioner?;
+  }
 
   void _sendMessage() async {
     if (_messageController.text.isNotEmpty) {
       await _chatService.sendMessages(
-          widget.receiverUid, _messageController.text);
+        widget.receiverUid,
+        _messageController.text,
+      );
       _messageController.clear();
     }
   }
@@ -29,14 +69,25 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.receiverUid)),
+      appBar: AppBar(
+        title: FutureBuilder<String>(
+            future: Future<String>.value(_receiverPractitioner!.name),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(' - ${snapshot.data!}');
+            } else {
+              return const Text('Loading...');
+            }
+          },
+        ),
+      ),
       body: Column(
         children: [
-          // messages
+          // Messages
           Expanded(
             child: _buildMessageList(),
           ),
-          // user input
+          // User input
           _buildMessageInput(),
         ],
       ),
@@ -47,7 +98,9 @@ class _ChatPageState extends State<ChatPage> {
   Widget _buildMessageList() {
     return StreamBuilder<List<Message>>(
       stream: _chatService.getMessages(
-          widget.receiverUid, _firebaseAuth.currentUser!.uid),
+        widget.receiverUid,
+        _firebaseAuth.currentUser!.uid,
+      ),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
@@ -61,8 +114,22 @@ class _ChatPageState extends State<ChatPage> {
 
         return ListView.builder(
           itemCount: messages.length,
-          itemBuilder: (context, index) {
-            return _buildMessageItem(messages[index]);
+            itemBuilder: (context, index) {
+              if (messages[index] == null) {
+                return const Text('No messages yet');
+              }
+              return FutureBuilder<Widget>(
+                future: _buildMessageItem(messages[index]),
+                builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return snapshot.data!;
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            );
           },
         );
       },
@@ -70,12 +137,15 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   // build message item
-  Widget _buildMessageItem(Message message) {
-    var alignment = (message.senderUid == _firebaseAuth.currentUser!.uid)
-        ? Alignment.centerRight
-        : Alignment.centerLeft;
-    return Container(
-      alignment: alignment,
+  Future<Widget> _buildMessageItem(Message message) async {
+    String senderName = _currentPractitioner?.name ?? 'Unknown';
+    String receiverName = _receiverPractitioner?.name ?? 'Unknown';
+
+  var alignment = (message.senderUid == _firebaseAuth.currentUser!.uid)
+      ? Alignment.centerRight
+      : Alignment.centerLeft;
+  return Container(
+    alignment: alignment,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -84,7 +154,7 @@ class _ChatPageState extends State<ChatPage> {
                   ? CrossAxisAlignment.end
                   : CrossAxisAlignment.start,
           children: [
-            Text(message.senderUid),
+            Text(senderName),
             Text(message.content),
           ],
         ),
@@ -92,24 +162,31 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
+
   // build input
   Widget _buildMessageInput() {
-    return Row(
-      children: [
-        Expanded(
-          child: TextField(
-            controller: _messageController,
-            obscureText: false,
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        color: Color(0xFFDADFEC),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              obscureText: false,
+            ),
           ),
-        ),
-        IconButton(
-          onPressed: _sendMessage,
-          icon: const Icon(
-            Icons.send,
-            size: 40,
-          ),
-        )
-      ],
+          IconButton(
+            onPressed: _sendMessage,
+            icon: const Icon(
+              Icons.send,
+              size: 40,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
